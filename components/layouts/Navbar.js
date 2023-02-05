39f2bb47a11d754cdd8eea25905ff87a677f0160
John Elmo Johnson <elmoj6@gmail.com>
2023-02-05 15:44:46 +1100

    Nav title link

--- components/layouts/Navbar.js
+++ components/layouts/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import { MdFastfood } from "react-icons/md";
 import Search from "../forms/Search";
@@ -7,10 +8,10 @@ const Navbar = () => {
   return (
     <div className="bg-primary py-3 text-white shadow sticky top-0 z-50">
       <div className="wrapper flex items-center justify-between">
-        <div className="flex items-center space-x-2">
+        <Link href="/" className="flex items-center space-x-2">
           <MdFastfood className="text-3xl" />
           <h1 className="text-2xl font-bold md:block hidden">Good Foods</h1>
-        </div>
+        </Link>
         <Search />
       </div>
     </div>
