01f12e59e41c714044870d038917802d67911275
John Elmo Johnson <elmoj6@gmail.com>
2023-02-05 15:46:51 +1100

    Added page for a category

---
+++ pages/category/[id].js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '@/components/layouts/Layout'
+
+const Category = () => {
+  return (
+    <Layout>Category</Layout>
+  )
+}
+
+export default Category
