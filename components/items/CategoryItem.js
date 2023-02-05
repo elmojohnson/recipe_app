a77c5d4f51bf395462bfbc40338b6083351a75e4
John Elmo Johnson <elmoj6@gmail.com>
2023-02-05 15:24:15 +1100

    Some UI update

--- components/items/CategoryItem.js
+++ components/items/CategoryItem.js
@@ -4,20 +4,24 @@ import React from "react";
 
 const CategoryItem = ({ category, description, thumbnail }) => {
   return (
-    <div className="bg-white shadow rounded-xl overflow-hidden flex flex-col items-center">
-      <Image
-        src={thumbnail}
-        alt={category}
-        width={150}
-        height={150}
-        priority
-        className={`h-auto w-full bg-white ${category === "Breakfast" || category === "Goat" ? null : "p-2"}`}
-      />
-      <div className="p-4">
-        <Link href={"/category/" + category} className="font-bold text-xl hover:text-primary">{category}</Link>
-        <p className="line-clamp-3">{description}</p>
+    <Link href={`/category/${category}`}>
+      <div className="bg-white shadow rounded-xl overflow-hidden flex flex-col items-center">
+        <Image
+          src={thumbnail}
+          alt={category}
+          width={150}
+          height={150}
+          priority
+          className={`h-auto w-full bg-white ${
+            category === "Breakfast" || category === "Goat" ? null : "p-2"
+          }`}
+        />
+        <div className="p-4">
+          <h3 className="font-bold text-xl hover:text-primary">{category}</h3>
+          <p className="line-clamp-3">{description}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
