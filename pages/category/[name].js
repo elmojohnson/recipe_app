be16b694d895f80920ded0871fc49960bdb2a8c7
John Elmo Johnson <elmoj6@gmail.com>
2023-02-06 16:19:41 +1100

    Moved meal list to a component

--- pages/category/[name].js
+++ pages/category/[name].js
@@ -2,10 +2,7 @@ import React from "react";
 import useCategoryByName from "@/hooks/useCategoryByName";
 
 import Layout from "@/components/layouts/Layout";
-import GridWrapper from "@/components/utils/GridWrapper";
-import MealItem from "@/components/items/MealItem";
-import MealLoadingItem from "@/components/items/MealLoadingItem";
-import { AnimatePresence } from "framer-motion";
+import MealList from "@/components/lists/MealList";
 
 const Category = () => {
   const { mealName, meals, isLoading } = useCategoryByName();
@@ -13,28 +10,7 @@ const Category = () => {
   return (
     <Layout>
       <h1 className="font-bold text-2xl mb-4">{mealName}</h1>
-      <AnimatePresence>
-        {isLoading ? (
-          <GridWrapper>
-            {[...Array(10)].map((_, i) => {
-              return <MealLoadingItem key={i} />;
-            })}
-          </GridWrapper>
-        ) : (
-          <GridWrapper>
-            {meals.map((meal) => {
-              return (
-                <MealItem
-                  key={meal.idMeal}
-                  id={meal.idMeal}
-                  meal={meal.strMeal}
-                  thumbnail={meal.strMealThumb}
-                />
-              );
-            })}
-          </GridWrapper>
-        )}
-      </AnimatePresence>
+      <MealList meals={meals} isLoading={isLoading} />
     </Layout>
   );
 };
