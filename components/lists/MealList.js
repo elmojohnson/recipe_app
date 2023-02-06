57b1ef331d299e6d18463394e817252dae185171
John Elmo Johnson <elmoj6@gmail.com>
2023-02-06 16:19:14 +1100

    Added component for meal list

---
+++ components/lists/MealList.js
@@ -0,0 +1,33 @@
+import GridWrapper from "@/components/utils/GridWrapper";
+import MealItem from "@/components/items/MealItem";
+import MealLoadingItem from "@/components/items/MealLoadingItem";
+import { AnimatePresence } from "framer-motion";
+
+const MealList = ({ meals, isLoading }) => {
+  return (
+    <AnimatePresence>
+      {isLoading ? (
+        <GridWrapper>
+          {[...Array(10)].map((_, i) => {
+            return <MealLoadingItem key={i} />;
+          })}
+        </GridWrapper>
+      ) : (
+        <GridWrapper>
+          {meals.map((meal) => {
+            return (
+              <MealItem
+                key={meal.idMeal}
+                id={meal.idMeal}
+                meal={meal.strMeal}
+                thumbnail={meal.strMealThumb}
+              />
+            );
+          })}
+        </GridWrapper>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default MealList;
