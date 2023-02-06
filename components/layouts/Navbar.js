f400aebcaa358f19dcefe0b7f399ee564346d28a
John Elmo Johnson <elmoj6@gmail.com>
2023-02-06 16:37:19 +1100

    Update: Nav title color on hover

--- components/layouts/Navbar.js
+++ components/layouts/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div className="bg-primary py-3 text-white shadow sticky top-0 z-50">
       <div className="wrapper flex items-center justify-between">
-        <Link href="/" className="flex items-center space-x-2">
+        <Link href="/" className="flex items-center space-x-2 hover:brightness-90">
           <MdFastfood className="text-3xl" />
           <h1 className="text-2xl font-bold md:block hidden">Good Foods</h1>
         </Link>
