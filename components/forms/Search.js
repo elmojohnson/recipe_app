1277348c6b353bff42ef8526d1f99f58146c87ad
John Elmo Johnson <elmoj6@gmail.com>
2023-02-06 16:15:32 +1100

    Update SearchBar component logic

--- components/forms/Search.js
+++ components/forms/Search.js
@@ -1,14 +1,19 @@
+import useSearch from "@/hooks/useSearch";
 import React from "react";
 import { MdSearch } from "react-icons/md";
 
-const Search = ({ value, onChange, handleSubmit }) => {
+const Search = () => {
+  const { query, handleChange, handleSubmit } = useSearch();
+  
   return (
     <div className="flex items-center space-x-3 justify-between bg-accent rounded-full py-1 px-3">
       <input
         placeholder="Search"
         className="bg-transparent outline-none placeholder:text-orange-200"
+        value={query}
+        onChange={handleChange}
       />
-      <button className="outline-none">
+      <button className="outline-none disabled:text-primary" disabled={!query} onClick={handleSubmit}>
         <MdSearch />
       </button>
     </div>
